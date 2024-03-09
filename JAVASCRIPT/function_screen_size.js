8043e30953811233ee0823a8513cdc211e80ce2c
Sasas <zipppycole@gmail.com>
2024-03-09 22:52:36 +0800

    Today's Recipe

--- JAVASCRIPT/function_screen_size.js
+++ JAVASCRIPT/function_screen_size.js
@@ -6,20 +6,68 @@ function changeResolutionState()
     // const var of height
     const screenHeight = window.screen.height;
 
+
+    var stylesheet_stylesheet, stylesheet_dialog, stylesheet_comment, stylesheet_food;  
+
     // check if the screen is hdtv
     if (screenWidth == 1280)
     {
         // change the href of the css to fhd but 4:3 resolution
-        document.getElementById('stylesheet').href = "../CSS/front-page-screen-fhd.css";
-        document.getElementById('stylesheet-dialog').href = "../CSS/front-page-dialog-box.css";
+        stylesheet_stylesheet = document.getElementById('stylesheet');
+        stylesheet_dialog = document.getElementById('stylesheet-dialog');
+        stylesheet_comment = document.getElementById('stylesheet-comment');
+        stylesheet_food = document.getElementById('stylesheet-food');
+        
+        if(stylesheet_stylesheet)
+        {
+            stylesheet_stylesheet.href = "../CSS/front-page-screen-fhd.css";
+        }
+
+        if(stylesheet_dialog)
+        {
+            stylesheet_dialog.href = "../CSS/front-page-dialog-box.css";
+        }
+
+        if(stylesheet_comment)
+        {
+            stylesheet_comment.href = "../CSS/food_comment_section.css";
+        }
+
+        if(stylesheet_food)
+        {
+            stylesheet_food.href = "../CSS/food_design.css";
+        }
+
     }
 
     //check if the screen is fhd but 16:9 resolution
     else if (screenWidth == 1920)
     {
         // change the href of the css to fhd
-        document.getElementById('stylesheet').href = "../CSS/front-page-screen-fhd-16-9.css";
-        document.getElementById('stylesheet-dialog').href = "../CSS/front-page-dialog-box-16-9.css";
+        stylesheet_stylesheet = document.getElementById('stylesheet');
+        stylesheet_dialog = document.getElementById('stylesheet-dialog');
+        stylesheet_comment = document.getElementById('stylesheet-comment');
+        stylesheet_food = document.getElementById('stylesheet-food');
+        
+        if(stylesheet_stylesheet)
+        {
+            stylesheet_stylesheet.href = "../CSS/front-page-screen-fhd-16-9.css";
+        }
+
+        if(stylesheet_dialog)
+        {
+            stylesheet_dialog.href = "../CSS/front-page-dialog-box-16-9.css";
+        }
+
+        if(stylesheet_comment)
+        {
+            stylesheet_comment.href = "../CSS/food_comment_section-16-9.css";
+        }
+
+        if(stylesheet_food)
+        {
+            stylesheet_food.href = "../CSS/food_design-16-9.css";
+        }
     }
 }
 
