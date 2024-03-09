21b5b8b217407171abbb56df4a16557b0fe87cee
Sasas <zipppycole@gmail.com>
2024-03-09 22:16:26 +0800

    Today's Recipe

--- JAVASCRIPT/function_screen_size.js
+++ JAVASCRIPT/function_screen_size.js
@@ -7,17 +7,19 @@ function changeResolutionState()
     const screenHeight = window.screen.height;
 
     // check if the screen is hdtv
-    if (screenWidth == 1366)
+    if (screenWidth == 1280)
     {
-        // change the href of the css to hd
+        // change the href of the css to fhd but 4:3 resolution
         document.getElementById('stylesheet').href = "../CSS/front-page-screen-fhd.css";
+        document.getElementById('stylesheet-dialog').href = "../CSS/front-page-dialog-box.css";
     }
 
-    //check if the screen is fhd
+    //check if the screen is fhd but 16:9 resolution
     else if (screenWidth == 1920)
     {
         // change the href of the css to fhd
-        document.getElementById('stylesheet').href = "../CSS/front-page-screen-fhd.css";
+        document.getElementById('stylesheet').href = "../CSS/front-page-screen-fhd-16-9.css";
+        document.getElementById('stylesheet-dialog').href = "../CSS/front-page-dialog-box-16-9.css";
     }
 }
 
